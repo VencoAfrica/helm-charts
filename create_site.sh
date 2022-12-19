e5a3c5ed77be6d20d33f6b17c51b6187a51d55d6
Paul Oluwaseun <devqxz@gmail.com>
2022-12-19 12:40:28 +0100

    revert python image to default erpnext worker

---
+++ create_site.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+curl -X POST https://sc.venco.africa/bench-command \
+    -H 'Content-Type: application/json' \
+    -u '132af98a-5b9d-40f1-8b93-8fe7d6365624:MuKpjDYkBQ5XttQLikzlYoHurUiTG4uE41E2XRXWhQfGRxv6MX89yG8teHpqlUI0'\
+    -d '{
+  "job_name": "create-site-'$1'",
+  "sites_pvc": "frappe-bench-erpnext",
+  "args": [
+    "bench",
+    "new-site",
+    "--admin-password=admin",
+    "--mariadb-root-username=root",
+    "--mariadb-root-password=root-password",
+    "--force",
+    "--install-app erpnext",
+    "--install-app estate_manager",
+    "--install-app mpesa_integration",
+    "--install-app paystack_integration",
+    "--install-app meter_management",
+    "--install-app simple_payments",
+    "--no-mariadb-socket",
+    "site-'$1'.venco.africa"
+  ],
+  "command": null,
+  "logs_pvc": null,
+  "namespace": "preprod",
+  "worker_image": "us-central1-docker.pkg.dev/venco-inc/frappe-worker/frappe-worker:venco_v0.0.333",
+  "nginx_image": "us-central1-docker.pkg.dev/venco-inc/frappe-nginx/frappe-nginx:v12.3",
+  "populate_assets": true
+}'
---
+++ create_sites.sh
@@ -0,0 +1,4 @@
+for current_num in {1..1}
+do
+./create_site.sh $current_num
+done
