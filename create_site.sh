2b83431635ccffeeb97e663715a91c08cc0f8b96
Paul Oluwaseun <devqxz@gmail.com>
2022-12-26 21:09:28 +0100

    revert python image to default erpnext worker

--- create_site.sh
+++ create_site.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
 
-curl -X POST https://sc.venco.africa/bench-command \
+curl -X POST http://0.0.0.0:8500/bench-command \
     -H 'Content-Type: application/json' \
-    -u '132af98a-5b9d-40f1-8b93-8fe7d6365624:MuKpjDYkBQ5XttQLikzlYoHurUiTG4uE41E2XRXWhQfGRxv6MX89yG8teHpqlUI0'\
+    -u '4b028616-078f-4f55-8d05-be4bc209ed5d:ObBL0blEyJqBXgXU0nR9vOGq6hjms9hk5ReovdnxvHGzDF2fVjZ1GAGCUQkZYE62'\
     -d '{
   "job_name": "create-site'$1'-job",
   "sites_pvc": "frappe-bench-erpnext",
   "args": [
     "bench",
     "new-site",
+    "site'$1'.venco.africa",
     "--admin-password=admin",
     "--mariadb-root-username=root",
-    "--mariadb-root-password=root-password",
-    "--force",
-    "--install-app erpnext",
-    "--install-app estate_manager",
-    "--install-app mpesa_integration",
-    "--install-app paystack_integration",
-    "--install-app meter_management",
-    "--install-app simple_payments",
-    "--install-app control_central_client",
+    "--db-host=db.venco.africa",
+    "--mariadb-root-password=all0wm3n0t",
     "--no-mariadb-socket",
-    "site'$1'.venco.africa"
+    "--no-mariadb-socket",
+    "--force"
+  
   ],
   "command": null,
   "logs_pvc": null,
   "namespace": "preprod",
-  "worker_image": "us-central1-docker.pkg.dev/venco-inc/frappe-worker/frappe-worker:venco_v0.0.666",
+  "worker_image": "us-central1-docker.pkg.dev/venco-inc/frappe-worker/frappe-worker:venco_build_v1",
   "nginx_image": "us-central1-docker.pkg.dev/venco-inc/frappe-nginx/frappe-nginx:v12.3",
   "populate_assets": true
 }'
